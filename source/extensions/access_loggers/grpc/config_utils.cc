f356bc340a6a5288a83aa663196565b5a5c64345
Dhi Aurrahman <dio@tetrate.io>
2019-11-20 23:23:37 +0000

    Fix
    Signed-off-by: Dhi Aurrahman <dio@tetrate.io>

--- source/extensions/access_loggers/grpc/config_utils.cc
+++ source/extensions/access_loggers/grpc/config_utils.cc
@@ -15,7 +15,7 @@ SINGLETON_MANAGER_REGISTRATION(grpc_access_logger_cache);
 std::shared_ptr<GrpcCommon::GrpcAccessLoggerCache>
 getGrpcAccessLoggerCacheSingleton(Server::Configuration::FactoryContext& context) {
   std::string overridden_downstream_ip;
-  for (const auto& field : context_.localInfo().node().metadata().fields()) {
+  for (const auto& field : context.localInfo().node().metadata().fields()) {
     if (field.first == "OVERRIDDEN_DOWNSTREAM_IP" &&
         field.second.kind_case() == ProtobufWkt::Value::kStringValue) {
       overridden_downstream_ip = field.second.string_value();
@@ -24,7 +24,7 @@ getGrpcAccessLoggerCacheSingleton(Server::Configuration::FactoryContext& context
   }
 
   return context.singletonManager().getTyped<GrpcCommon::GrpcAccessLoggerCache>(
-      SINGLETON_MANAGER_REGISTERED_NAME(grpc_access_logger_cache), [&context] {
+      SINGLETON_MANAGER_REGISTERED_NAME(grpc_access_logger_cache), [&context, overridden_downstream_ip] {
         return std::make_shared<GrpcCommon::GrpcAccessLoggerCacheImpl>(
             context.clusterManager().grpcAsyncClientManager(), context.scope(),
             context.threadLocal(), context.localInfo(), overridden_downstream_ip);
